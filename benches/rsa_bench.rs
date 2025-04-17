d30e6002a4dedcf4c0bb76360cdbc8aaf3d3f4a7
nichokas <nicolas.rodalv@educa.jcyl.es>
2025-04-17 14:10:02 +0200

    Fix RSA

--- benches/rsa_bench.rs
+++ benches/rsa_bench.rs
@@ -5,14 +5,18 @@ fn generate_keypair() {
     let _rsa = Rsa::generate(7680).unwrap();
 }
 
-fn round_trip(keys: Rsa<openssl::pkey::Private>){
+fn round_trip(keys: Rsa<openssl::pkey::Private>) {
     let message = b"Secret message";
 
     let mut encrypt_buf = vec![0; keys.size() as usize];
     let encrypted_len = keys.public_encrypt(message, &mut encrypt_buf, Padding::PKCS1).unwrap();
     encrypt_buf.truncate(encrypted_len);
+
     let mut decrypt_buf = vec![0; keys.size() as usize];
-    let _decrypted_len = keys.private_decrypt(&encrypt_buf, &mut decrypt_buf, Padding::PKCS1).unwrap();
+    let decrypted_len = keys.private_decrypt(&encrypt_buf, &mut decrypt_buf, Padding::PKCS1).unwrap();
+    decrypt_buf.truncate(decrypted_len);
+
+    assert_eq!(message, &decrypt_buf[..]);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
