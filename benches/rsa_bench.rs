e887e2d19cbdb895ce96cbbe02fd2d0aad1a53cd
nichokas <nicolas.rodalv@educa.jcyl.es>
2025-04-17 12:27:51 +0200

    Finished RSA encryption

--- benches/rsa_bench.rs
+++ benches/rsa_bench.rs
@@ -2,13 +2,17 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use openssl::rsa::{Rsa, Padding};
 
 fn generate_keypair() {
-    let rsa = Rsa::generate(7680).unwrap();
+    let _rsa = Rsa::generate(7680).unwrap();
 }
 
 fn round_trip(keys: Rsa<openssl::pkey::Private>){
     let message = b"Secret message";
-    let mut buf = vec![0; keys.size() as usize];
-    let _encrypted_len = keys.public_encrypt(message, &mut buf, Padding::PKCS1).unwrap();
+    
+    let mut encrypt_buf = vec![0; keys.size() as usize];
+    let encrypted_len = keys.public_encrypt(message, &mut encrypt_buf, Padding::PKCS1).unwrap();
+    encrypt_buf.truncate(encrypted_len);
+    let mut decrypt_buf = vec![0; keys.size() as usize];
+    let _decrypted_len = keys.private_decrypt(&encrypt_buf, &mut decrypt_buf, Padding::PKCS1).unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
